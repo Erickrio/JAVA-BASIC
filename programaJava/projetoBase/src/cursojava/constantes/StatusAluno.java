0bbcb946aa05bc7f223cc003ee4678a1c1a421ef
erick Monteiro <monteiro.ck@hotmail.com>
2020-10-26 21:16:18 -0300

    constantes

--- programaJava/projetoBase/src/cursojava/constantes/StatusAluno.java
+++ programaJava/projetoBase/src/cursojava/constantes/StatusAluno.java
@@ -3,7 +3,7 @@ package cursojava.constantes;
 public class StatusAluno {
 
 	/*tem o mesmo valor e serve para todo o sistema
-	 * Classes Static e Constantes
+	 * Classes Static e Constantes - ENUM
 	*/
 	public static String APROVADO = "Aprovado";
 	public static String RECUPERACAO = "Recuperacao";
